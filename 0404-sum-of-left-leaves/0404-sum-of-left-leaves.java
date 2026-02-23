b335be875c451e8d353a0081d04d53c0b0337348
Dipanshu Adhikari <121121532+Dipansh01@users.noreply.github.com>
2026-02-23 19:02:33 +0530

    Time: 0 ms (100%), Space: 43.4 MB (29.53%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -12,18 +12,18 @@
  *         this.right = right;
  *     }
  * }
- */
+ */ 
 class Solution {
+    int sum = 0;
     public int sumOfLeftLeaves(TreeNode root){
         if(root == null){
             return 0;
         }
-        int sum = 0;
         if(root.left!=null && (root.left.left==null && root.left.right==null)){
             sum += root.left.val;
         }
-        sum += sumOfLeftLeaves(root.left);
-        sum += sumOfLeftLeaves(root.right);
+        sumOfLeftLeaves(root.left);
+        sumOfLeftLeaves(root.right);
         return sum;
     }
 }
