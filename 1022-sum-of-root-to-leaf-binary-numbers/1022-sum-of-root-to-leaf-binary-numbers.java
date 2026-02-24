2cf6e2be3c180189918df81cd437d27e7ccfe676
Dipanshu Adhikari <121121532+Dipansh01@users.noreply.github.com>
2026-02-24 22:45:42 +0530

    Time: 6 ms (5.1%), Space: 44.4 MB (12.01%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -26,6 +26,7 @@ class Solution {
         str += root.val;
         if(root.left==null && root.right==null){
             sum += Integer.parseInt(str,2);
+            return;
         }
         dfs(root.left,str);
         dfs(root.right,str);
