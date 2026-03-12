c4f221ec959f810aafa491d0cbacb5287de56b29
Dipanshu Adhikari <121121532+Dipansh01@users.noreply.github.com>
2026-03-12 19:00:16 +0530

    Time: 17 ms (92.87%), Space: 48 MB (99.27%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.java
+++ 3163-string-compression-iii/3163-string-compression-iii.java
@@ -1,32 +1,31 @@
 class Solution {
     public String compressedString(String word) {
         char arr[] = word.toCharArray();
-        String ans = "";
+        StringBuilder ans = new StringBuilder();
         int i=0,j;
         for(j=0;j<arr.length;j++){
             if(arr[i] != arr[j]){
                 int freq = j-i;
                 while(freq > 9){
-                    ans += "9" + arr[i];
+                    ans.append("9").append(arr[i]);
                     freq = freq-9;
                 }
                 if(freq != 0){
-                    ans += freq;
-                    ans += arr[i];
+                    ans.append(freq);
+                    ans.append(arr[i]);
                 }
                 i = j;
             }
         }
         int freq = j-i;
         while(freq > 9){
-            ans += "9" + arr[i];
+            ans.append("9").append(arr[i]);
             freq = freq-9;
         }
         if(freq != 0){
-            ans += freq;
-            ans += arr[i];
+            ans.append(freq);
+            ans.append(arr[i]);
         }
-        i = j;
-        return ans;
+        return ans.toString();
     }
 }
