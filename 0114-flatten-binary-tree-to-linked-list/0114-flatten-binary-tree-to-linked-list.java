e2e11fcb498074b3b4950effe8d1a6d7d8f8f2c5
Dipanshu Adhikari <121121532+Dipansh01@users.noreply.github.com>
2026-08-22 23:31:08 +0530

    Time: 0 ms (100%), Space: 44 MB (53.48%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -18,24 +18,16 @@ class Solution {
         if(root == null){
             return;
         }
-        ArrayList<TreeNode> arr = new ArrayList<>();
-        dfs(root,arr);
-        for(int i=0;i<arr.size()-1;i++){
-            TreeNode a = arr.get(i);
-            TreeNode b = arr.get(i+1);
-            a.right = b;
-            a.left = null;
+        TreeNode leftSubTree = root.left;
+        TreeNode rightSubTree = root.right;
+        flatten(leftSubTree);
+        flatten(rightSubTree);
+        root.right = leftSubTree;
+        root.left = null;
+        TreeNode temp = root;
+        while(temp.right != null){
+            temp = temp.right;
         }
-        TreeNode last = arr.get(arr.size()-1);
-        last.left = null;
-        last.right = null;
-    }
-    public void dfs(TreeNode root, ArrayList<TreeNode> arr){
-        if(root == null){
-            return;
-        }
-        arr.add(root);
-        dfs(root.left,arr);
-        dfs(root.right,arr);
+        temp.right = rightSubTree;
     }
 }
