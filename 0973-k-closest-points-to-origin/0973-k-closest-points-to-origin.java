764f500fd54635d7d7641cd35e2e6484d0e14666
Dipanshu Adhikari <121121532+Dipansh01@users.noreply.github.com>
2026-09-07 02:17:46 +0530

    Time: 50 ms (11.8%), Space: 58.6 MB (22.8%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,37 @@
+class Triplet implements Comparable<Triplet>{
+    int dis;
+    int x;
+    int y;
+
+    Triplet(int dis, int x, int y){
+        this.dis = dis;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int compareTo(Triplet t){
+        return this.dis - t.dis;
+    }
+}
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Triplet> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int[] point : points){
+            int x = point[0];
+            int y = point[1];
+            int dis = x*x + y*y;
+            pq.add(new Triplet(dis,x,y));
+
+            if(pq.size() > k){
+                pq.remove();
+            }
+        }
+        int ans[][] = new int[k][2];
+        for(int i=0;i<k;i++){
+            Triplet top = pq.remove();
+            ans[i][0] = top.x;
+            ans[i][1] = top.y;
+        }
+        return ans;
+    }
+}
