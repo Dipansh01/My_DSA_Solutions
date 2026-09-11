ad997c96d51013e14a8467b30c0b0953c0f4eaf6
Dipanshu Adhikari <121121532+Dipansh01@users.noreply.github.com>
2026-09-12 04:28:07 +0530

    Time: 2 ms (47.33%), Space: 46.5 MB (25.84%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n = rooms.size();
+        boolean[] vis = new boolean[n];
+        vis[0] = true;
+        bfs(rooms, vis, 0);
+        for(int i=0;i<n;i++){
+            if(vis[i] == false){
+                return false;
+            }
+        }
+        return true;
+    }
+    public void bfs(List<List<Integer>> rooms, boolean[] vis, int st){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(st);
+        while(!q.isEmpty()){
+            int front = q.remove();
+            for(int ele : rooms.get(front)){
+                if(!vis[ele]){
+                    q.add(ele);
+                    vis[ele] = true;
+                }
+            }
+        }
+    }
+}
