a56b983e153d99804b95cf3349536961476ef6d6
Dipanshu Adhikari <121121532+Dipansh01@users.noreply.github.com>
2026-09-14 01:48:14 +0530

    Time: 3 ms (87.42%), Space: 52.2 MB (66.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,61 +1,34 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int res = 0;
         int m = grid.length;
         int n = grid[0].length;
         boolean[][] vis = new boolean[m][n];
+        int res = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(!vis[i][j] && grid[i][j] == '1'){
-                    bfs(grid, vis, i, j);
+                    dfs(grid, vis, i, j);
                     res++;
                 }
             }
         }
         return res;
     }
-    public void bfs(char[][] grid, boolean[][] vis, int i, int j){
+    public void dfs(char[][] grid, boolean[][] vis, int i, int j){
         int m = grid.length;
         int n = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{i,j});
         vis[i][j] = true;
-        while(!q.isEmpty()){
-            int[] front = q.remove();
-            int row = front[0];
-            int col = front[1];
-
-            // top
-            if(row-1 >= 0){
-                if(!vis[row-1][col] && grid[row-1][col] == '1'){
-                    q.add(new int[]{row-1,col});
-                    vis[row-1][col] = true;
-                }
-            }
-
-            // bottom
-            if(row+1 < m){
-                if(!vis[row+1][col] && grid[row+1][col] == '1'){
-                    q.add(new int[]{row+1,col});
-                    vis[row+1][col] = true;
-                }
-            }
-
-            // left
-            if(col-1 >= 0){
-                if(!vis[row][col-1] && grid[row][col-1] == '1'){
-                    q.add(new int[]{row,col-1});
-                    vis[row][col-1] = true;
-                }
-            }
-
-            // right
-            if(col+1 < n){
-                if(!vis[row][col+1] && grid[row][col+1] == '1'){
-                    q.add(new int[]{row,col+1});
-                    vis[row][col+1] = true;
-                }
-            }
+        if(i-1 >= 0 && grid[i-1][j] == '1' && !vis[i-1][j]){
+            dfs(grid, vis, i-1, j);
+        }
+        if(i+1 <= m-1 && grid[i+1][j] == '1' && !vis[i+1][j]){
+            dfs(grid, vis, i+1, j);
+        }
+        if(j-1 >= 0 && grid[i][j-1] == '1' && !vis[i][j-1]){
+            dfs(grid, vis, i, j-1);
+        }
+        if(j+1 <= n-1 && grid[i][j+1] == '1' && !vis[i][j+1]){
+            dfs(grid, vis, i, j+1);
         }
     }
 }
