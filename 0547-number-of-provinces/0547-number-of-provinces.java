74c0b465b59b7b7f480acc53d3c97f13358e678d
Dipanshu Adhikari <121121532+Dipansh01@users.noreply.github.com>
2026-09-14 00:18:32 +0530

    Time: 0 ms (100%), Space: 47.4 MB (43.43%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,33 +1,22 @@
 class Solution {
     public int findCircleNum(int[][] adj) {
         int n = adj.length;
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adjList.add(new ArrayList<>());
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i != j && adj[i][j] == 1){
-                    adjList.get(i).add(j);
-                    adjList.get(j).add(i);
-                }
-            }
-        }
         boolean[] vis = new boolean[n];
         int res = 0;
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                dfs(adjList, vis, i);
+                dfs(adj, vis, i);
                 res++;
             }
         }
         return res;
     }
-    public void dfs(ArrayList<ArrayList<Integer>> adjList, boolean[] vis, int i){
+    public void dfs(int[][] adj, boolean[] vis, int i){
+        int n = adj.length;
         vis[i] = true;
-        for(int num : adjList.get(i)){
-            if(!vis[num]){
-                dfs(adjList, vis, num);
+        for(int j=0;j<n;j++){
+            if(adj[i][j] == 1 && !vis[j]){
+                dfs(adj, vis, j);
             }
         }
     }
