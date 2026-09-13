162dcf634455e8d6dcceffac3839c76cd3bdcbd5
Dipanshu Adhikari <121121532+Dipansh01@users.noreply.github.com>
2026-09-14 02:40:51 +0530

    Time: 176 ms (16.28%), Space: 309.3 MB (9.57%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -14,23 +14,14 @@ class Solution {
             adj.get(b).add(a);
         }
         boolean[] vis = new boolean[n];
-        bfs(adj, vis, source, destination);
+        dfs(adj, vis, source, destination);
         return vis[destination];
     }
-    public void bfs(List<List<Integer>> adj, boolean[] vis, int st, int ed){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(st);
+    public void dfs(List<List<Integer>> adj, boolean[] vis, int st, int ed){ 
         vis[st] = true;
-        while(!q.isEmpty()){
-            int front = q.remove();
-            for(int ele : adj.get(front)){
-                if(!vis[ele]){
-                    q.add(ele);
-                    vis[ele] = true;
-                    if(ele == ed){
-                        return;
-                    }
-                }
+        for(int num : adj.get(st)){
+            if(!vis[num]){
+                dfs(adj, vis, num, ed);
             }
         }
     }
