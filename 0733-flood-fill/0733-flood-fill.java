7acb52d4b513604a47d20b4b4dad0103b24cf7e6
Dipanshu Adhikari <121121532+Dipansh01@users.noreply.github.com>
2026-09-15 00:28:05 +0530

    Time: 0 ms (100%), Space: 46.9 MB (42.63%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,34 +1,20 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m = image.length;
-        int n = image[0].length;
         if(image[sr][sc] != color){
-            int prev = image[sr][sc];
-            image[sr][sc] = color;
-            Queue<int[]> q = new LinkedList<>();
-            q.add(new int[]{sr,sc});
-            while(!q.isEmpty()){
-                int[] front = q.remove();
-                int i = front[0];
-                int j = front[1];
-                if(i-1 >= 0 && image[i-1][j] == prev){
-                    q.add(new int[]{i-1,j});
-                    image[i-1][j] = color;
-                }
-                if(i+1 < m && image[i+1][j] == prev){
-                    q.add(new int[]{i+1,j});
-                    image[i+1][j] = color;
-                }
-                if(j-1 >= 0 && image[i][j-1] == prev){
-                    q.add(new int[]{i,j-1});
-                    image[i][j-1] = color;
-                }
-                if(j+1 < n && image[i][j+1] == prev){
-                    q.add(new int[]{i,j+1});
-                    image[i][j+1] = color;
-                }
-            }
+            dfs(image, sr, sc, color, image[sr][sc]);
         }
         return image;
+    }
+    public void dfs(int[][] image, int sr, int sc, int color, int prevColor){
+        int m = image.length;
+        int n = image[0].length;
+        if(sr < 0 || sr > m-1 || sc < 0 || sc > n-1 || image[sr][sc] != prevColor || image[sr][sc] == color){
+            return;
+        }
+        image[sr][sc] = color;
+        dfs(image,sr-1,sc,color,prevColor);
+        dfs(image,sr,sc-1,color,prevColor);
+        dfs(image,sr+1,sc,color,prevColor);
+        dfs(image,sr,sc+1,color,prevColor);
     }
 }
