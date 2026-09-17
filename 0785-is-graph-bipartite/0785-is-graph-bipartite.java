26f7a12c19569eabaa6397a7a5baf5a4d44a0357
Dipanshu Adhikari <121121532+Dipansh01@users.noreply.github.com>
2026-09-17 15:51:00 +0530

    Time: 1 ms (63.83%), Space: 47.2 MB (57.06%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,56 +1,32 @@
 class Solution {
-
-    private boolean isSafe(int node, int[] color, int[][] graph, int targetColor) {
-
-        for (int neighbor : graph[node]) {
-            if (color[neighbor] == targetColor) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private boolean solve(int node, int[] color, int[][] graph) {
-
-        for (int i = 1; i <= 2; i++) {
-
-            if (isSafe(node, color, graph, i)) {
-
-                color[node] = i;
-
-                for (int neighbor : graph[node]) {
-
-                    if (color[neighbor] == 0) {
-
-                        if (!solve(neighbor, color, graph)) {
-                            return false;
-                        }
-                    }
-                }
-
-                return true;
-            }
-        }
-
-        return false;
-    }
-
+    static boolean res;
     public boolean isBipartite(int[][] graph) {
-
-        int N = graph.length;
-        int[] color = new int[N];
-
-        for (int i = 0; i < N; i++) {
-
-            if (color[i] == 0) {
-
-                if (!solve(i, color, graph)) {
-                    return false;
+        res = true;
+        int n = graph.length;
+        int[] vis = new int[n];
+        for(int i=0;i<n;i++){
+            if(vis[i] == 0){
+                bfs(graph, vis, i);
+            }
+        }
+        return res;
+    }
+    public static void bfs(int[][] graph, int[] vis, int i){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+        vis[i] = 1;
+        while(!q.isEmpty()){
+            int front = q.remove();
+            for(int ele : graph[front]){
+                if(vis[ele] == vis[front]){
+                    res = false;
+                    return;
+                }
+                if(vis[ele] == 0){
+                    q.add(ele);
+                    vis[ele] = (vis[front] == 1) ? 2 : 1;
                 }
             }
         }
-
-        return true;
     }
 }
