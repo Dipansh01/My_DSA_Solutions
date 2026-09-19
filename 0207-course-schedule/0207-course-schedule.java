14058921af99a22ddf0bb780057003a17be09cba
Dipanshu Adhikari <121121532+Dipansh01@users.noreply.github.com>
2026-09-19 16:33:29 +0530

    Time: 7 ms (56.82%), Space: 47.4 MB (21.13%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adjList.add(new ArrayList<>());
+        }
+        for(int i=0;i<prerequisites.length;i++){
+            int a = prerequisites[i][0];
+            int b = prerequisites[i][1];
+            adjList.get(a).add(b);
+        }
+        int[] inDegree = new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            for(int num : adjList.get(i)){
+                inDegree[num]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(inDegree[i] == 0){
+                q.add(i);
+            }
+        }
+        int count = 0;
+        while(!q.isEmpty()){
+            int front = q.remove();
+            count++;
+            for(int num : adjList.get(front)){
+                inDegree[num]--;
+                if(inDegree[num] == 0){
+                    q.add(num);
+                }
+            }
+        }
+        return count == numCourses;
+    }
+}
